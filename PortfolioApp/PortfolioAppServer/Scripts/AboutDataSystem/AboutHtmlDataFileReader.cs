f4228b7ad24d1fc6531abb898b940bb7e859afee
Artem <zumikq78@gmail.com>
2022-08-07 18:55:28 +0200

    Refactor: AboutHtmlDataFileReaders format fix

--- PortfolioApp/PortfolioAppServer/Scripts/AboutDataSystem/AboutHtmlDataFileReader.cs
+++ PortfolioApp/PortfolioAppServer/Scripts/AboutDataSystem/AboutHtmlDataFileReader.cs
@@ -3,12 +3,12 @@ namespace PortfolioAppServer.AboutDataSystem;
 public class AboutHtmlDataFileReader : IAboutDataProvider
 {
     private readonly string _filePath;
-    
+
     public AboutHtmlDataFileReader(string filePath)
     {
         _filePath = filePath;
     }
-    
+
     public string GetData()
     {
         return File.ReadAllText(_filePath);
